c234155a1b0fbf2af76b8bc7fb41c5f728312915
Ticaj-0 <thierrycaj@gmail.com>
2024-08-05 15:32:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,3 +101,26 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Affichez une UI pour inviter l'utilisateur à installer l'application
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
