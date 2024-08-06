4656809665a641a67c0702ccd1ca8a68a5cd2fab
Ticaj-0 <thierrycaj@gmail.com>
2024-08-06 15:14:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -93,13 +93,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
-        }, function(err) {
-            console.log('Service Worker registration failed:', err);
-        });
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(err) {
+      console.log('Service Worker registration failed:', err);
     });
+  });
 }
 
 let deferredPrompt;
